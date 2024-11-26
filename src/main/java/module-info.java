9d5ffd9f1603178b9be6a674982d1150a2d044a6
S.M.D.C.S.Setunge <chirathsetunge@gmail.com>
2024-11-26 16:22:30 +0530

    dependency updates (pom and module-info.java)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.example.ood_cw {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
+    requires mysql.connector.j;
 
 
     opens Frontend to javafx.fxml;
