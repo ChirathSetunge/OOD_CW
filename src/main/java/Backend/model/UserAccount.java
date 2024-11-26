05a5a0a3540404bfe3953f00b678ed01f111e6be
S.M.D.C.S.Setunge <chirathsetunge@gmail.com>
2024-11-26 16:18:21 +0530

    Update UserAccount.java (initial, Login, SignUp)

--- src/main/java/Backend/model/UserAccount.java
+++ src/main/java/Backend/model/UserAccount.java
@@ -1,5 +1,18 @@
 package Backend.model;
 
-public class UserAccount extends Account {
+import Backend.service.DatabaseHandler;
 
+public class UserAccount extends Account {
+    private UserPreferences userPreferences;
+
+    public UserAccount(String username, String password, String email) {
+        super(username, password, email);
+        userPreferences = new UserPreferences();
+    }
+    public boolean register(DatabaseHandler dbHandler) {
+        return dbHandler.registerAccount(this);
+    }
+    public boolean login(DatabaseHandler dbHandler) {
+        return dbHandler.loginAccount(this);
+    }
 }
