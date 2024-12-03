aaa674098bfa3463fa97c223083b07a71fc1cb78
S.M.D.C.S.Setunge <chirathsetunge@gmail.com>
2024-12-03 13:03:09 +0530

    Updated SignUp and Login controllers

--- src/main/java/Frontend/LoginController.java
+++ src/main/java/Frontend/LoginController.java
@@ -3,15 +3,28 @@ package Frontend;
 
 import Backend.model.UserAccount;
 import Backend.service.DatabaseHandler;
-import Backend.util.ConcurrencyManager;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class LoginController {
+    @FXML
+    private Label infoPanel;
+    @FXML
+    private Button startSceneButton;
+    @FXML
+    private Button exitButton;
     @FXML
     private TextField loginUsername;
     @FXML
@@ -22,20 +35,89 @@ public class LoginController {
     private Button loginButton;
 
     private final DatabaseHandler databaseHandler = DatabaseHandler.getDbInstance();
-    private final ConcurrencyManager concurrencyManager = new ConcurrencyManager();
-
 
     @FXML
-    public void loginButtonOnAction(ActionEvent event) {
+    private void onLoginButtonClicked(ActionEvent event) {
+        //login process (getting the credentials from the text fields)
         String username = loginUsername.getText();
         String password = loginPassword.getText();
         if (username.isEmpty() || password.isEmpty()) {
-            System.out.println("Please fill in all fields");
+            infoPanel.setText("Please fill in all fields");
+            System.out.println("User did not fill in all fields");
             return;
         }
+        // login process (checking the credentials)
         UserAccount userAccount = new UserAccount(username, password, null);
-        concurrencyManager.loginAccount(userAccount, databaseHandler);
-        //add a label in the fxml to show the user that he has logged in
+        boolean loginSuccess = userAccount.login(databaseHandler);
+        if (loginSuccess) {
+            infoPanel.setText("Login successful!");
+            System.out.println("User login successful!");
+            // Move to the next scene (ViewArticle)
+            navigateToViewArticle(event, userAccount);
+        } else {
+            infoPanel.setText("Login failed! Please check your username and password or already logged in.");
+            System.out.println("User login failed! error in username and password.");
+        }
+
+    }
+
+    private void navigateToViewArticle(ActionEvent event, UserAccount userAccount) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ViewArticle.fxml"));
+            Parent root = fxmlLoader.load();
+            // Pass the userAccount
+            ViewArticleController viewArticleController = fxmlLoader.getController();
+            viewArticleController.setCurrentUser(userAccount);
+            // Move to the next scene in a new window
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            infoPanel.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the ViewArticle scene.");
+        }
+    }
+    @FXML
+    private void navigateToSignUp(ActionEvent event) {
+        // Move to the signUp scene
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SignUp.fxml")); // Adjust the FXML path
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the SignUp scene.");
+        }
+    }
+    @FXML
+    private void navigateToStartScene(ActionEvent event) {
+        // Move to the start scene
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the Start scene.");
+        }
+    }
+    @FXML
+    private void onStartSceneButtonClicked(ActionEvent event) {
+        // Move to the start scene
+        navigateToStartScene(event);
+    }
+    @FXML
+    private void onExitButtonClicked(ActionEvent event) {
+        // Exit the application
+        Platform.exit();
     }
 
 
--- src/main/java/Frontend/SignUpController.java
+++ src/main/java/Frontend/SignUpController.java
@@ -2,39 +2,120 @@ package Frontend;
 
 import Backend.model.UserAccount;
 import Backend.service.DatabaseHandler;
-import Backend.util.ConcurrencyManager;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignUpController {
     @FXML
+    private Label infoPanel;
+    @FXML
     private Button signUpButton;
-    @FXML
-    private TextField signUpUsername;
-    @FXML
-    private TextField signUpPassword;
     @FXML
     private Button loginButton;
     @FXML
-    private TextField signUpEmail;
-
-
+    private Button startSceneButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private TextField registerPassword;
+    @FXML
+    private TextField registerUsername;
+    @FXML
+    private TextField registerEmail;
     private final DatabaseHandler databaseHandler = DatabaseHandler.getDbInstance();
-    private final ConcurrencyManager concurrencyManager = new ConcurrencyManager();
 
     @FXML
-    public void signUpButtonOnAction(ActionEvent event) {
-        String username = signUpUsername.getText();
-        String password = signUpPassword.getText();
-        String email = signUpEmail.getText();
+    public void onSignUpButtonClicked(ActionEvent event) {
+        //register process (getting the credentials from the text fields)
+        String username = registerUsername.getText();
+        String password = registerPassword.getText();
+        String email = registerEmail.getText();
         if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
-            System.out.println("Please fill in all fields");
+            infoPanel.setText("Please fill in all fields");
+            System.out.println("New user did not fill in all fields");
             return;
         }
+        //register process (checking the credentials)
         UserAccount userAccount = new UserAccount(username, password, email);
-        concurrencyManager.registerAccount(userAccount, databaseHandler);
-        //add a label in the fxml to show the user that the account has been created
+        boolean registerSuccess = userAccount.register(databaseHandler);
+        if (registerSuccess) {
+            infoPanel.setText("Account registered successfully!");
+            System.out.println("New account registered successfully!");
+            // Move to the next scene (ViewArticle)
+            navigateToViewArticle(event, userAccount);
+        } else {
+            infoPanel.setText("Account registration failed!, username already exists.");
+        }
     }
+    private void navigateToViewArticle(ActionEvent event, UserAccount userAccount) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ViewArticle.fxml"));
+            Parent root = fxmlLoader.load();
+            // Pass the userAccount
+            ViewArticleController viewArticleController = fxmlLoader.getController();
+            viewArticleController.setCurrentUser(userAccount);
+            // Move to the next scene in a new window
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            infoPanel.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the ViewArticle scene.");
+        }
+    }
+    @FXML
+    private void navigateToLogin(ActionEvent event) {
+        // Move to the login scene
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml")); // Adjust the FXML path
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the Login scene.");
+        }
+    }
+    @FXML
+    private void navigateToStartScene(ActionEvent event) {
+        // Move to the start scene
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the Start scene.");
+        }
+    }
+    @FXML
+    private void onStartSceneButtonClicked(ActionEvent event) {
+        // Move to the start scene
+        navigateToStartScene(event);
+    }
+    @FXML
+    private void onExitButtonClicked(ActionEvent event) {
+        // Exit the application
+        Platform.exit();
+    }
+
 }
